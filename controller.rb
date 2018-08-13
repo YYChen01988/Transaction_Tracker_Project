d192d0918b0781e3d2847a11c8c49ef9263e6d39
yingying <chen.yingying.01988@gmail.com>
2018-08-13 14:56:50 +0100

    refactore CURD in Tag and Merchant

--- controller.rb
+++ controller.rb
@@ -8,27 +8,27 @@ require_relative ( './models/tag.rb')
 
 also_reload ( './models/*')
 
-
+#List of Transactions
 get '/transactions' do
   @transactions = Transaction.all()
   @tags = Tag.all()
   @merchants = Merchant.all()
   erb (:index)
 end
-
+#New
 get '/transactions/new' do
   @merchants = Merchant.all()
   @tags = Tag.all()
   erb(:new)
 end
-
+#Create
 post '/transactions' do
   params["transaction_time"] = Time.now.asctime
-  @transactions =  Transaction.new(params)
-  @transactions.save()
+  @transaction =  Transaction.new(params)
+  @transaction.save()
   erb(:create)
 end
-
+#Delete
 get '/transactions/:id/delete' do
   @transaction = Transaction.find(params[:id])
   @transaction.delete()
@@ -50,4 +50,82 @@ post '/transactions/:id' do
   @transaction = Transaction.new(params)
   @transaction.update()
   redirect '/transactions'
+end
+
+
+
+#List of Merchants
+get '/merchants' do
+  @merchants = Merchant.all()
+  erb (:merchant_index)
+end
+#Delete Merchant
+get '/merchants/:id/delete' do
+  @merchant = Merchant.find_by_id(params[:id])
+  @merchant.delete_by_id([:id])
+  redirect '/merchants'
+end
+#New
+get '/merchants/new' do
+  erb(:merchant_new)
+end
+#Create
+post '/merchants' do
+  @merchant = Merchant.new(params)
+  @merchant.save()
+  redirect '/merchants'
+end
+
+# Edit
+get '/merchants/:id/edit' do
+  @merchants = Merchant.all()
+  @merchant = Merchant.find_by_id(params[:id])
+  erb(:merchant_edit)
+end
+
+# Update
+post '/merchants/:id' do
+  @merchant = Merchant.new(params)
+  @merchant.update()
+  redirect '/merchants'
+end
+
+
+
+
+
+#List of Tags
+get '/tags' do
+  @tags = Tag.all()
+  erb (:tag_index)
+end
+#Delete
+get '/tags/:id/delete' do
+  @tag = Tag.find_by_id(params[:id])
+  @tag.delete_by_id([:id])
+  redirect '/tags'
+end
+#New
+get '/tags/new' do
+  erb(:tag_new)
+end
+#Create
+post '/tags' do
+  @tag = Tag.new(params)
+  @tag.save()
+  redirect '/tags'
+end
+
+# Edit
+get '/tags/:id/edit' do
+  @tags = Tag.all()
+  @tag = Tag.find_by_id(params[:id])
+  erb(:tag_edit)
+end
+
+# Update
+post '/tags/:id' do
+  @tag = Tag.new(params)
+  @tag.update()
+  redirect '/tags'
 end
